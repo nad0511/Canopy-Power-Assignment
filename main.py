71453ebaf9ac4c5d561c2fb0d39686272b4be7c6
nad0511 <59272955+nad0511@users.noreply.github.com>
2022-02-09 17:28:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy
+import pandas
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv('data.csv')
+
+
+null_data = df[df.isnull().any(axis=1)]
+
+# print(df)
+
+total_time = df['irradiance'].count()
+
+
+df = df.interpolate(method ='linear', limit_direction ='forward')
+
+df['PV'] = df['irradiance'] * 0.09
+
+
+for index in df.index:
+    if df.iloc[index]['PV'] > df.iloc[index]['total_load_kw']:
+        df.loc[index,'PV'] = df.iloc[index].at['total_load_kw']
+
+df['genset'] = df['total_load_kw'] - df['PV']
+
+
+null_data = df[df.isnull().any(axis=1)]
+
+
+total_littre = sum(df['genset']) /60 * 0.3
+
+print("total litres of diesel: ", total_littre)
+
+df.plot(x="timestamp", y=["PV", "total_load_kw", "genset"])
+
+plt.show()
+
+
